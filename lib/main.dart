57ada1efcfa8c1d71de5fbea69b6f1db96991309
nativacu <nativacu@gmail.com>
2022-06-14 20:52:28 -0400

    feature: adding xylophone buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -11,14 +12,71 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
+          child: Column(children: [
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.red)),
+              onPressed: () {
+                player.play('note1.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.orange)),
+              onPressed: () {
+                player.play('note2.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.yellow)),
+              onPressed: () {
+                player.play('note3.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green)),
+              onPressed: () {
+                player.play('note4.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.teal)),
+              onPressed: () {
+                player.play('note5.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue)),
+              onPressed: () {
+                player.play('note6.wav');
+              },
+              child: Text(''),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple)),
               onPressed: () {
                 player.play('note7.wav');
               },
-              child: Text('click me'),
+              child: Text(''),
             ),
-          ),
+          ]),
         ),
       ),
     );
